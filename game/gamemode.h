0a730fd9303766cb6557721c902740779b227101
unknown <uo7@njit.edu>
2019-12-12 16:51:29 -0500

    Added this file which includes static cooldown floats

--- game/gamemode.h
+++ game/gamemode.h
@@ -1,2 +1,4 @@
-#pragma once
 extern int gamemode;
+static float cldn_wleap, cldn_mcroll, cldn_flash, cldn_scatter, cldn_blink;
+static float _wleap = 6, _mcroll = 6, _flash = 10, _scatter = 10, _blink = 3;
+//static int num_blinks;
