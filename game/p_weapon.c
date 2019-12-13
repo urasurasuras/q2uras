8319ea57dbc27aaa02ae38c954d94fa1f264468c
Uras Oran <uo7@njit.edu>
2019-12-13 09:46:25 -0500

    Added Pharah as rocketlauncher

--- game/p_weapon.c
+++ game/p_weapon.c
@@ -802,7 +802,7 @@ void Weapon_RocketLauncher (edict_t *ent)
 {
 	static int	pause_frames[]	= {25, 33, 42, 50, 0};
 	static int	fire_frames[]	= {5, 0};
-	//SetHero(ent);
+	ent->client->pers.hero = "Pharah";
 	Weapon_Generic (ent, 4, 12, 50, 54, pause_frames, fire_frames, Weapon_RocketLauncher_Fire);
 }
 
